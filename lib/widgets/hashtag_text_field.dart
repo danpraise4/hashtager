439192fcc199e6f282aef45dee16d2b804170aa9
Dan-Possible <31796765+danpraise4@users.noreply.github.com>
2024-06-21 14:31:51 +0100

    update

--- lib/widgets/hashtag_text_field.dart
+++ lib/widgets/hashtag_text_field.dart
@@ -1022,8 +1022,7 @@ class _HashTagTextFieldState extends State<HashTagTextField>
       return effectiveDecoration.copyWith(
         errorText: effectiveDecoration.errorText ?? '',
         counterStyle: effectiveDecoration.errorStyle ??
-            themeData.textTheme.titleLarge!
-                .copyWith(color: themeData.primaryColor),
+            themeData.textTheme.bodySmall!.copyWith(color: Colors.red),
         counterText: counterText,
         semanticCounterText: semanticCounterText,
       );
@@ -1196,7 +1195,8 @@ class _HashTagTextFieldState extends State<HashTagTextField>
     final ThemeData theme = Theme.of(context);
     final TextSelectionThemeData selectionTheme =
         TextSelectionTheme.of(context);
-    final TextStyle style = theme.textTheme.bodySmall!.merge(widget.basicStyle);
+    final TextStyle style =
+        theme.textTheme.titleMedium!.merge(widget.basicStyle);
     final Brightness keyboardAppearance =
         widget.keyboardAppearance ?? theme.brightness;
     final TextEditingController controller = _effectiveController;
